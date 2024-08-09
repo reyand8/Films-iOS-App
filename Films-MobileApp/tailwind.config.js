28a0bc4c368a6a794f91ffe096b5c5f278f06927
Andrey <andreeey.gg@gmail.com>
2024-08-09 12:48:23 +0200

    add tailwind config

---
+++ Films-MobileApp/tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ["./App.{js,jsx,ts,tsx}", "./screen/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}",
+        "./navigation/**/*.{js,jsx,ts,tsx}"],
+    theme: {},
+    plugins: [],
+}
