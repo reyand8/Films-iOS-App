cdfc84a5d6dd6b0f6de74847f95c98bdbe124deb
Andrey <andreeey.gg@gmail.com>
2024-08-09 12:45:32 +0200

    order imports

--- Films-MobileApp/App.js
+++ Films-MobileApp/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from "./navigation/AppNavigation";
 import {ApolloClient, ApolloProvider, from, HttpLink, InMemoryCache} from "@apollo/client";
 
+import AppNavigation from "./navigation/AppNavigation";
 
 
 export default function App() {
